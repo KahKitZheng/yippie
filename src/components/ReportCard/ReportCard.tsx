bbdefdf006d67054367503115c4ccf88d4bf9a35
Kah Kit <kahkitzheng@gmail.com>
2024-03-15 22:43:28 +0100

    Feat: Add more colours + use fake data

--- src/components/ReportCard/ReportCard.tsx
+++ src/components/ReportCard/ReportCard.tsx
@@ -1,7 +1,46 @@
 import { useEffect, useState } from "react";
 
-export default function ReportCard() {
+type ReportCardProps = {
+  report: any;
+};
+
+export default function ReportCard(props: ReportCardProps) {
+  const { report } = props;
+
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  function translateStatus() {
+    const status = report.status;
+    switch (status) {
+      case "draft":
+        return "Concept";
+      case "submitted":
+        return "Gepubliceerd";
+      case "approved":
+        return "Goedgekeurd";
+      case "rejected":
+        return "Afgekeurd";
+      default:
+        return "Onbekend";
+    }
+  }
+
+  function statusStyle() {
+    const status = report.status;
+
+    switch (status) {
+      case "draft":
+        return "bg-neutral-300";
+      case "submitted":
+        return "bg-sky-300";
+      case "approved":
+        return "bg-emerald-300";
+      case "rejected":
+        return "bg-rose-300";
+      default:
+        return "bg-neutral-300";
+    }
+  }
 
   function renderRelatedMembers() {
     const numberOfMembers = 2;
@@ -16,7 +55,7 @@ export default function ReportCard() {
 
     return Array.from({ length: numberOfMembers }).map((_) => (
       <div className="flex items-center gap-2">
-        <span className="hidden h-4 w-4 place-content-center rounded-full bg-neutral-200 p-3 text-sm sm:grid">
+        <span className="hidden h-4 w-4 place-content-center rounded-full border border-indigo-200 bg-indigo-100 p-3 text-indigo-900 sm:grid">
           J
         </span>
         <p>John doe</p>
@@ -34,12 +73,12 @@ export default function ReportCard() {
   }, []);
 
   return (
-    <div className="grid gap-4 rounded-md border border-slate-200 p-3 hover:border-black sm:p-4">
+    <div className="group grid gap-4 rounded-md border border-slate-200 p-3 hover:border-indigo-700 hover:bg-indigo-50 sm:p-4">
       <div className="flex items-baseline justify-between sm:gap-4">
-        <p className="text-base font-bold">Bila gesprek John</p>
+        <p className="text-base font-semibold">{report.name}</p>
         <p className="inline-flex items-center gap-2 sm:justify-end">
-          <span className="h-2 w-2 rounded-full bg-slate-300" />
-          <span className="hidden sm:inline">Goedgekeurd</span>
+          <span className={`${statusStyle()} h-[10px] w-[10px] rounded-full`} />
+          <span className="hidden sm:inline">{translateStatus()}</span>
         </p>
       </div>
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,55 +1,25 @@
 import { FaPlus } from "react-icons/fa6";
+import reportsData from "../../fake-data/reports.json";
 import Layout from "../../components/Layout";
 import ReportCard from "../../components/ReportCard";
 
 export default function HomePage() {
-  const teamMembers = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-  ];
-
   return (
     <Layout>
-      <div className="grid gap-4 border-l-neutral-200 py-12 sm:border-l">
-        <div className="-ml-[6.5px] grid grid-cols-[200px_1fr] items-baseline">
-          <div></div>
-          <div className="hidden items-center justify-between gap-4 sm:flex">
-            <div className="relative isolate flex flex-row-reverse flex-wrap">
-              {teamMembers
-                // .splice(0, 5)
-                .map((member, index) => (
-                  <button
-                    key={member}
-                    className={`relative z-[${(teamMembers.length - index) * 10}] grid h-4 w-4 select-none place-content-center rounded-full border border-neutral-200 bg-white p-4 text-sm font-medium hover:z-[100] hover:border-black [&:not(:last-child)]:-ml-2`}
-                  >
-                    {member}
-                  </button>
-                ))
-                .reverse()}
-            </div>
-            <button className="flex items-center gap-2 rounded-md bg-black px-3 py-2 font-bold text-white">
-              <FaPlus />
-              <span>nieuw verslag</span>
-            </button>
-          </div>
+      <div className="grid gap-4 border-l-slate-100 py-12 sm:border-l">
+        <div className="flex justify-end gap-4">
+          <button className="flex items-center gap-2 rounded-md bg-indigo-950 px-3 py-2 font-bold text-white">
+            <FaPlus />
+            <span>nieuw verslag</span>
+          </button>
         </div>
-        {Array.from({ length: 30 }).map((_, index) => (
+        {reportsData.map((report, index) => (
           <div className="sm:-ml-[6.5px] sm:grid sm:grid-cols-[200px_1fr] sm:items-baseline">
             <div className="hidden items-center gap-3 sm:flex">
-              <span className="block h-3 w-3 rounded-full bg-neutral-300" />
+              <span className="block h-3 w-3 rounded-full bg-indigo-200" />
               <p>{new Date().toLocaleDateString().replace(/\//g, "-")}</p>
             </div>
-            <ReportCard key={index} />
+            <ReportCard key={index} report={report} />
           </div>
         ))}
       </div>
