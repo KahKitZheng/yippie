356d49223371c564f1908c1b8f2416ddc2961735
Kah Kit <kahkitzheng@gmail.com>
2024-03-23 17:19:30 +0100

    Feat: Add button to delete question

--- src/pages/ReportPage/ReportPage.tsx
+++ src/pages/ReportPage/ReportPage.tsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import Layout from "../../components/Layout";
 import TipTap from "../../components/TipTap";
 import { JSONContent } from "@tiptap/react";
-import { FaPlus } from "react-icons/fa6";
+import { FaGripVertical, FaPlus, FaXmark } from "react-icons/fa6";
+import { randomId } from "../../utils";
 
 type ReportData = {
+  id: string;
   question: { placeholder: string; data: JSONContent };
   answer: { placeholder: string; data: JSONContent };
 };
 
 const baseTemplate: ReportData[] = [
   {
+    id: "z3pjWB6Utn",
     question: {
       placeholder: "Verslag naam",
       data: {
@@ -37,6 +40,7 @@ const baseTemplate: ReportData[] = [
     },
   },
   {
+    id: "khAN2qUjMb",
     question: {
       placeholder: "What’s the best way to waste time?",
       data: {} as JSONContent,
@@ -47,6 +51,7 @@ const baseTemplate: ReportData[] = [
     },
   },
   {
+    id: "fasKdyHh3c",
     question: {
       placeholder: "What’s your favorite playlist?",
       data: {} as JSONContent,
@@ -57,6 +62,7 @@ const baseTemplate: ReportData[] = [
     },
   },
   {
+    id: "mi4Vdx4dM7",
     question: {
       placeholder: "What is one thing you think is really overrated?",
       data: {} as JSONContent,
@@ -71,15 +77,11 @@ const baseTemplate: ReportData[] = [
 export default function ReportPage() {
   const [report, setReport] = useState(baseTemplate);
 
-  function updateField(index: number, content: Record<string, JSONContent>) {
-    const copyReport = [...report];
-    copyReport[index].question.data = content.question.data;
-
-    setReport(copyReport);
-  }
+  // console.log(randomId());
 
   function addQuestion() {
     const newQuestion: ReportData = {
+      id: randomId(),
       question: {
         placeholder: "What’s one thing you’ve always wanted to learn?",
         data: {} as JSONContent,
@@ -93,31 +95,52 @@ export default function ReportPage() {
     setReport([...report, newQuestion]);
   }
 
+  function updateQuestion(index: number, content: JSONContent) {
+    const copyReport = [...report];
+    copyReport[index].question.data = content;
+
+    setReport(copyReport);
+  }
+
+  function removeQuestion(id: string) {
+    const cleanReport = report.filter((data) => data.id !== id);
+
+    setReport(cleanReport);
+  }
+
   return (
     <Layout includePadding>
       <div className="grid gap-8">
         {report.map((data, index) => (
-          <TipTap
-            key={index}
-            content={data.question.data}
-            updateContent={() => updateField(index, data)}
-            variant={index === 0 ? "editor-title" : "editor"}
-            placeholder={data.question.placeholder}
-          />
+          <div key={data.id} className="-mx-8 flex items-center gap-4">
+            <button>
+              <FaGripVertical />
+            </button>
+            <TipTap
+              content={data.question.data}
+              updateContent={(content: JSONContent) =>
+                updateQuestion(index, content)
+              }
+              variant={index === 0 ? "editor-title" : "editor"}
+              placeholder={data.question.placeholder}
+            />
+            <button onClick={() => removeQuestion(data.id)}>
+              <FaXmark />
+            </button>
+          </div>
         ))}
-
-        <button
-          className="mx-auto mt-8 flex items-center justify-center gap-2 rounded-3xl border border-slate-200 bg-white px-4 py-2"
-          onClick={addQuestion}
-        >
-          <FaPlus />
-          vraag toevoegen
-        </button>
-
-        <button className="mx-auto mt-8 flex w-60 items-center justify-center rounded bg-indigo-950 p-3 font-medium text-white">
-          opslaan
-        </button>
       </div>
+      <button
+        className="mx-auto mt-8 flex items-center justify-center gap-2 rounded-3xl border border-slate-200 bg-white px-4 py-2"
+        onClick={addQuestion}
+      >
+        <FaPlus />
+        vraag toevoegen
+      </button>
+
+      <button className="mx-auto mt-16 flex w-60 items-center justify-center rounded bg-indigo-950 p-3 font-medium text-white">
+        opslaan
+      </button>
     </Layout>
   );
 }
