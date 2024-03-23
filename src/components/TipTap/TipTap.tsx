25b787f26c7f8ebcf7259ed1989326e8786a06ec
Kah Kit <kahkitzheng@gmail.com>
2024-03-23 16:37:05 +0100

    Feat: Add basic layout for editing a report template

--- src/components/TipTap/TipTap.tsx
+++ src/components/TipTap/TipTap.tsx
@@ -1,29 +1,81 @@
-import { EditorProvider, BubbleMenu } from "@tiptap/react";
+import {
+  useEditor,
+  EditorContent,
+  JSONContent,
+  BubbleMenu,
+} from "@tiptap/react";
+import Link from "@tiptap/extension-link";
+import Document from "@tiptap/extension-document";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
-import "./TipTap.css";
+import "./TipTap.scss";
+import { useEffect } from "react";
 
 type TipTapProps = {
-  content: string;
-  editable?: boolean;
+  variant: "editor" | "editor-title";
+  content: JSONContent;
+  updateContent?: (content: JSONContent) => void;
   placeholder?: string;
+  isEditing?: boolean;
 };
 
 export default function TipTap(props: TipTapProps) {
-  const { content, editable = true } = props;
+  const {
+    variant = "editor",
+    content,
+    updateContent,
+    placeholder,
+    isEditing = true,
+  } = props;
 
-  const extensions = [
-    StarterKit,
-    Placeholder.configure({ placeholder: "Type something" }),
-  ];
+  const editor = useEditor({
+    content: "",
+    editable: isEditing,
+    extensions: [
+      StarterKit.configure({
+        document: false,
+      }),
+      Document.extend({
+        content: variant === "editor-title" ? "heading block*" : undefined,
+      }),
+      Placeholder.configure({
+        placeholder: placeholder ?? "",
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+      }),
+    ],
+    onUpdate: ({ editor }) => {
+      if (updateContent === undefined) {
+        return;
+      }
+
+      updateContent(editor?.getJSON());
+    },
+    editorProps: {
+      attributes: {
+        class: `prose ${isEditing ? "editing" : ""}`,
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (Object.keys(content).length === 0) {
+      return;
+    }
+
+    editor?.commands.setContent(content);
+  }, [content, editor]);
 
   return (
-    <EditorProvider
-      extensions={extensions}
-      content={content}
-      editable={editable}
-    >
-      <BubbleMenu>This is the bubble menu</BubbleMenu>
-    </EditorProvider>
+    <>
+      <EditorContent editor={editor} className="tiptap-editor" />
+      {editor && (
+        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          This is the bubble menu
+        </BubbleMenu>
+      )}
+    </>
   );
 }
--- src/pages/ReportPage/ReportPage.tsx
+++ src/pages/ReportPage/ReportPage.tsx
@@ -1,12 +1,123 @@
+import { useState } from "react";
 import Layout from "../../components/Layout";
 import TipTap from "../../components/TipTap";
+import { JSONContent } from "@tiptap/react";
+import { FaPlus } from "react-icons/fa6";
+
+type ReportData = {
+  question: { placeholder: string; data: JSONContent };
+  answer: { placeholder: string; data: JSONContent };
+};
+
+const baseTemplate: ReportData[] = [
+  {
+    question: {
+      placeholder: "Verslag naam",
+      data: {
+        type: "doc",
+        content: [
+          {
+            type: "heading",
+            attrs: {
+              level: 1,
+            },
+            content: [
+              {
+                type: "text",
+                text: "Team platform - Bila",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    answer: {
+      placeholder: "",
+      data: {} as JSONContent,
+    },
+  },
+  {
+    question: {
+      placeholder: "What’s the best way to waste time?",
+      data: {} as JSONContent,
+    },
+    answer: {
+      placeholder: "",
+      data: {} as JSONContent,
+    },
+  },
+  {
+    question: {
+      placeholder: "What’s your favorite playlist?",
+      data: {} as JSONContent,
+    },
+    answer: {
+      placeholder: "",
+      data: {} as JSONContent,
+    },
+  },
+  {
+    question: {
+      placeholder: "What is one thing you think is really overrated?",
+      data: {} as JSONContent,
+    },
+    answer: {
+      placeholder: "",
+      data: {} as JSONContent,
+    },
+  },
+];
 
 export default function ReportPage() {
+  const [report, setReport] = useState(baseTemplate);
+
+  function updateField(index: number, content: Record<string, JSONContent>) {
+    const copyReport = [...report];
+    copyReport[index].question.data = content.question.data;
+
+    setReport(copyReport);
+  }
+
+  function addQuestion() {
+    const newQuestion: ReportData = {
+      question: {
+        placeholder: "What’s one thing you’ve always wanted to learn?",
+        data: {} as JSONContent,
+      },
+      answer: {
+        placeholder: "",
+        data: {} as JSONContent,
+      },
+    };
+
+    setReport([...report, newQuestion]);
+  }
+
   return (
     <Layout includePadding>
-      <p>ReportPage</p>
+      <div className="grid gap-8">
+        {report.map((data, index) => (
+          <TipTap
+            key={index}
+            content={data.question.data}
+            updateContent={() => updateField(index, data)}
+            variant={index === 0 ? "editor-title" : "editor"}
+            placeholder={data.question.placeholder}
+          />
+        ))}
 
-      <TipTap content="" />
+        <button
+          className="mx-auto mt-8 flex items-center justify-center gap-2 rounded-3xl border border-slate-200 bg-white px-4 py-2"
+          onClick={addQuestion}
+        >
+          <FaPlus />
+          vraag toevoegen
+        </button>
+
+        <button className="mx-auto mt-8 flex w-60 items-center justify-center rounded bg-indigo-950 p-3 font-medium text-white">
+          opslaan
+        </button>
+      </div>
     </Layout>
   );
 }
