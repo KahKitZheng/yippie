747727d7044c600cc7211bca14b87293593b32b8
Kah Kit <kahkitzheng@gmail.com>
2024-03-23 17:18:57 +0100

    Feat: Add util to generate random ids

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,20 @@
 export function formatLocalDate(date: Date) {
   return new Date(date).toLocaleDateString().replace(/\//g, "-");
 }
+
+export function randomId(length = 10) {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  const crypto = window.crypto;
+  const array = new Uint32Array(length);
+
+  return new Array(length)
+    .fill(0)
+    .map((_, index) =>
+      possible.charAt(
+        Math.floor(crypto.getRandomValues(array)[index] % possible.length),
+      ),
+    )
+    .join("");
+}
